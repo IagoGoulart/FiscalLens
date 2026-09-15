f68ab2383beb5373995fac9d7e777b44c7dfb0bd
Iago Goulart <iagogoulart15@gmail..com>
2026-09-14 21:24:25 -0300

    Corrige codificação da conexão com PostgreSQL

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,11 +1,12 @@
-const {Pool} = require("pg");
+const { Pool } = require("pg");
 
 const pool = new Pool({
     host: process.env.DB_HOST || "localhost",
     port: process.env.DB_PORT || 5432,
     database: process.env.DB_NAME,
     user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD 
+    password: process.env.DB_PASSWORD,
+    options: "-c client_encoding=UTF8"
 });
 
 module.exports = pool;
